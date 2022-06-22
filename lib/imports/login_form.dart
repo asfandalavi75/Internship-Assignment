fbc52616da6739d85dbeea7a7664ec15d9e6a1f6
asfandalavi75 <89604405+asfandalavi75@users.noreply.github.com>
2022-06-21 21:28:23 -0400

    LoginPage UI done
    Need to Implement Backend

---
+++ lib/imports/login_form.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool _isHidden = true;
+  Color emailColor = Colors.white;
+  Color passColor = Colors.white;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 20, top: 20, bottom: 5),
+            child: Text(
+              "Email",
+              style: GoogleFonts.lato(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3), //color of shadow
+                  spreadRadius: 5, //spread radius
+                  blurRadius: 7, // blur radius
+                  offset: Offset(0, 2), // changes position of shadow
+                  //first paramerter of offset is left-right
+                  //second parameter is top to down
+                ),
+                //you can set more BoxShadow() here
+              ],
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: emailColor,
+                width: 2,
+              ),
+            ),
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            child: TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return null;
+                }
+                return null;
+              },
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20, top: 20, bottom: 5),
+            child: Text(
+              "Password",
+              style: GoogleFonts.lato(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            height: MediaQuery.of(context).size.height * 0.07,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3), //color of shadow
+                  spreadRadius: 5, //spread radius
+                  blurRadius: 7, // blur radius
+                  offset: Offset(0, 2), // changes position of shadow
+                  //first paramerter of offset is left-right
+                  //second parameter is top to down
+                ),
+              ],
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: passColor,
+                width: 2,
+              ),
+            ),
+            child: TextFormField(
+              controller: passwordController,
+              obscureText: _isHidden,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                suffix: InkWell(
+                  onTap: _togglePasswordView,
+                  child: Icon(
+                    _isHidden ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+                //eye icon if passenable = true, else, Icon is ***__
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {}
+                return null;
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                "Forgot Password?",
+                style: GoogleFonts.lato(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              height: MediaQuery.of(context).size.height * 0.07,
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: ElevatedButton(
+                onPressed: () {
+                  bool emailCheck = emailController.value.text.isNotEmpty;
+                  bool passCheck = passwordController.value.text.isNotEmpty;
+                  int emailLen = emailController.value.text.length;
+                  int passLen = passwordController.value.text.length;
+
+                  setState(() {
+                    if (emailCheck == true && emailLen > 8) {
+                      emailColor = Colors.white;
+                    } else {
+                      emailColor = Colors.red;
+                    }
+                    if (passCheck == true && passLen > 8) {
+                      passColor = Colors.white;
+                    } else {
+                      passColor = Colors.red;
+                    }
+                  });
+                },
+                child: Text(
+                  "Log In",
+                  style: GoogleFonts.lato(
+                    fontSize: 25,
+                  ),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+                margin: const EdgeInsets.only(top: 90),
+                height: MediaQuery.of(context).size.height * 0.07,
+                width: MediaQuery.of(context).size.width * 0.85,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "New user? Create account",
+                    style: GoogleFonts.lato(
+                      fontSize: 15,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        return Colors.white; // Use the component's default.
+                      },
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: const BorderSide(
+                              width: 2.0, color: Colors.indigo)),
+                    ),
+                  ),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,38 +2,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import './imports/background_shapes.dart';
 import 'package:google_fonts/google_fonts.dart';
+import './imports/login_form.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
+//   @override
+//   State<LoginPage> createState() => _LoginPageState();
+// }
 
-class _LoginPageState extends State<LoginPage> {
+// class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.25,
-            child: CustomPaint(painter: BackgroundShapes(), child: Container()),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 20, top: 20),
-            child: Text(
-              "Log In",
-              style: GoogleFonts.lato(
-                fontWeight: FontWeight.w900,
-                fontSize: 40,
-                color: Colors.indigo,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.25,
+              child:
+                  CustomPaint(painter: BackgroundShapes(), child: Container()),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Log In",
+                style: GoogleFonts.lato(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 40,
+                  color: Colors.indigo,
+                ),
               ),
             ),
-          )
-        ],
+            LoginForm(),
+          ],
+        ),
       ),
     );
   }
